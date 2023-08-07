23ee980e5bdefa08f841bcdf3e2eef8bf52a159e
Marceli Sobiecki <64848895+MrMarox@users.noreply.github.com>
2023-08-07 20:07:40 +0200

    build
    1.04

--- my-vue-app/src/modules/Home.tsx
+++ my-vue-app/src/modules/Home.tsx
@@ -2,6 +2,21 @@ import "../styles/Home.scss";
 
 export default function Home(){
   return (
-    <h1>HELLO WORLD</h1>
+    <div className="content">
+        <div className="first_container">
+            <div className="left">
+                <h1>Smart Transaction,
+                    Secured Privacy.
+                </h1>
+                <a>
+                    Pay everything with us to avoid data and privacy 
+                    leak to your personal credit card even all your banking accounts.
+                </a>
+            </div>
+            <div className="right">
+
+            </div>
+        </div>
+    </div>
   );
 }
