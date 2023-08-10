f3f4b0fae66244dfbc7d053121eb6888e3adcac9
Marceli Sobiecki <64848895+MrMarox@users.noreply.github.com>
2023-08-10 19:52:12 +0200

    update

--- my-vue-app/src/components/molecules/SecurityLeft.tsx
+++ my-vue-app/src/components/molecules/SecurityLeft.tsx
@@ -1,7 +1,7 @@
 export default function SecurityLeft({ location }: { location: string }) {
     return (
         <>
-            <img src={location} alt="website-logo" />
+            <img src={location} alt="website-logo1111" />
         </>
     );
 }
